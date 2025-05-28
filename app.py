a2050fa784e61ac1fa285dff37301cac2988d55e
Dhina <thinakaran0078@gmail.com>
2025-05-29 02:41:03 +0530

    api improvement basic error handling

--- app.py
+++ app.py
@@ -14,6 +14,11 @@ def index():
 @app.post("/store")
 def create_store():
     store_data = request.get_json()
+    if "name" not in store_data:
+        abort(400, message="Ensure Store name is Present")
+    for store in stores:
+        if store["name"] == store_data["name"]:
+            abort(400, message="Store already exists.")
     store_id = uuid.uuid4().hex
     store = {**store_data, "id": store_id}
     stores[store_id] = store
@@ -24,8 +29,19 @@ def create_store():
 @app.post("/item")
 def create_item():
     item_data = request.get_json()
+    if(
+        "price" not in item_data or "store_id" not in item_data or "name" not in item_data
+    ):
+        abort(400, message="Bad request. Ensure 'price', 'store_id', name are present.")
+    for item in items.values():
+        if (
+            item_data["name"] == item["name"]
+            and item_data["store_id"] == item["store_id"]
+        ):
+            abort(400, message="Item already exists.")
+
     if item_data["store_id"] not in stores:
-        return {"message": "Store not found"}, 404
+        abort(404, message="Store not found.")
 
     item_id = uuid.uuid4().hex
     item = {**item_data, "id": item_id}
@@ -39,14 +55,14 @@ def get_store(store_id):
     try:
         return stores[store_id]
     except KeyError:
-        return {"message": "Store not found"}, 404
+        abort(404, message="Store not found.")
 
 @app.get("/item/<string:item_id>")
 def get_item(item_id):
     try:
         return items[item_id]
     except KeyError:
-        return abort(404, message="Item not found")
+        abort(404, message="Item not found.")
 
 
 @app.get("/store")
