d5e5e5d6199e02da98e2e4a68efc3ee61c83691f
Dhina <thinakaran0078@gmail.com>
2025-07-30 02:28:55 +0530

    more SQLALCHEMY Enhancements added.

--- resources/item.py
+++ resources/item.py
@@ -2,7 +2,10 @@ import uuid
 from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
-from db import items
+from sqlalchemy.exc import SQLAlchemyError
+
+from db import db
+from models import ItemModel
 from schemas import ItemSchema, ItemsUpdateSchema
 
 blp = Blueprint("items", __name__, description="Operations on Items")
@@ -12,28 +15,18 @@ blp = Blueprint("items", __name__, description="Operations on Items")
 class Item(MethodView):
     @blp.response(200, ItemSchema)
     def get(self, item_id):
-        try:
-            return items[item_id]
-        except KeyError:
-            abort(404, message="Item not found.")
+        item = ItemModel.query.get_or_404(item_id)
+        return item
 
     def delete(self, item_id):
-        try:
-            del items[item_id]
-            return {"message": "Item deleted."}
-        except KeyError:
-            abort(404, message="Item not found.")
+        item = ItemModel.query.get_or_404(item_id)
+        raise NotImplementedError("Delete an item is not implemented yet.")
 
     @blp.arguments(ItemsUpdateSchema)
     @blp.response(200, ItemSchema)
     def put(self, item_data, item_id):
-        try:
-            item = items[item_id]
-            item |= item_data
-
-            return item
-        except KeyError:
-            abort(404, message="Item not found.")
+        item = ItemModel.query.get_or_404(item_id)
+        raise NotImplementedError("Updating an item is not implemented yet.")
 
 
 @blp.route("/item")
@@ -45,15 +38,12 @@ class ItemList(MethodView):
     @blp.arguments(ItemSchema)
     @blp.response(201, ItemSchema)
     def post(self, item_data):
-        for item in items.values():
-            if (
-                item_data["name"] == item["name"]
-                and item_data["store_id"] == item["store_id"]
-            ):
-                abort(400, message="Item already exists.")
+        item = ItemModel(**item_data)
 
-        item_id = uuid.uuid4().hex
-        item = {**item_data, "id": item_id}
-        items[item_id] = item
+        try:
+            db.session.add(item)
+            db.session.commit()
+        except SQLAlchemyError:
+            abort(500, message="An error occurred while creating the item.")
 
-        return item, 201
+        return item
--- resources/store.py
+++ resources/store.py
@@ -1,9 +1,14 @@
 import uuid
+
 from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
-from db import stores
+from sqlalchemy.exc import SQLAlchemyError, IntegrityError
+
 from schemas import StoreSchema
+
+from db import db
+from models import StoreModel
 
 blp = Blueprint("stores", __name__, description="Operations on Stores")
 
@@ -33,12 +38,14 @@ class StoreList(MethodView):
     @blp.arguments(StoreSchema)
     @blp.response(201, StoreSchema)
     def post(self, store_data):
-        for store in stores.values():
-            if store["name"] == store_data["name"]:
-                abort(400, message="Store already exists.")
+        store = StoreModel(**store_data)
 
-        store_id = uuid.uuid4().hex
-        store = {**store_data, "id": store_id}
-        stores[store_id] = store
+        try:
+            db.session.add(store)
+            db.session.commit()
+        except IntegrityError:
+            abort(400, message="A store with that name already exists.")
+        except SQLAlchemyError:
+            abort(500, message="An error occurred while creating the store.")
 
-        return store, 201
+        return store
