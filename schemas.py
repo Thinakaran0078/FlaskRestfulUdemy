974e4e44388300869c4ade75be483cab8a59b7c9
Dhina <thinakaran0078@gmail.com>
2025-08-05 02:12:30 +0530

    schema changes and item

--- schemas.py
+++ schemas.py
@@ -15,6 +15,7 @@ class PlainStoreSchema(Schema):
 class ItemsUpdateSchema(Schema):
     name = fields.Str()
     price = fields.Float()
+    store_id = fields.Int()
 
 
 class ItemSchema(PlainItemSchema):
