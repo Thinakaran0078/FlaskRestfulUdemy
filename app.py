6bf7ab9fb4ec4113b4088cf6cc3af65ec844cac5
Dhina <thinakaran0078@gmail.com>
2025-08-24 00:54:28 +0530

    alchemy log off

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def create_app(db_url=None):
     app.config['OPENAPI_SWAGGER_UI_URL'] = 'https://cdn.jsdelivr.net/npm/swagger-ui-dist/'
     app.config['SQLALCHEMY_DATABASE_URI'] = db_url or os.getenv('DATABASE_URL', f'sqlite:///{db_file}')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config["SQLALCHEMY_ECHO"] = True
+    # app.config["SQLALCHEMY_ECHO"] = True
     db.init_app(app)
     migrate = Migrate(app, db)
     api = Api(app)
